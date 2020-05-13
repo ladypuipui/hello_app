73c79bb2b80bce79fab549b11affe19e55727e60
Natsumi Dakeshita <54981355+ladypuipui@users.noreply.github.com>
2020-05-13 21:01:12 +0900

    add: basicauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :basic
+    
+            def basic
+                authenticate_or_request_with_http_basic do |name, password|
+                    name == "hello" && password == "YU!vKHAfdCPv"
+            end
+        end
+
     def hello
         render html: "Hello, world!"
     end
